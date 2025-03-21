38bb737a197e890f87c3aa816ff8b48fe3484a19
Victor Fedyanin <fedyanin.v.v@yandex.ru>
2025-03-21 15:28:19 +0300

    Lession 2: - Add modules "kitchen"(8081), "waiter"(8080) - Add SpringBoot Run files to both modules - Resolved dependencies in children and parent POM's - Add spring-boot-actuator properties to both modules

---
+++ kitchen/src/main/java/com/education/kitchen/RunKitchen.java
@@ -0,0 +1,11 @@
+package com.education.kitchen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RunKitchen {
+    public static void main(String[] args) {
+        SpringApplication.run(RunKitchen.class, args);
+    }
+}
