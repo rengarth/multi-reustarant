bd814bbf4168d621d936ba909c7b40e6e1b6f055
Victor Fedyanin <fedyanin.v.v@yandex.ru>
2025-03-21 13:12:58 +0300

    Initial commit

---
+++ src/main/java/com/education/ligarestaurant/LigaRestaurantApplication.java
@@ -0,0 +1,13 @@
+package com.education.ligarestaurant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LigaRestaurantApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LigaRestaurantApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/education/ligarestaurant/LigaRestaurantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.education.ligarestaurant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LigaRestaurantApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
