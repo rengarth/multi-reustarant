5d217b0a07c978a5317997fbd04808c4d13cd99c
Victor Fedyanin <fedyanin.v.v@yandex.ru>
2025-03-26 15:27:51 +0300

    Development: - Add configs to consumer, producer and topic

---
+++ payment-service/src/main/java/com/education/paymentservice/config/KafkaTopicConfig.java
@@ -0,0 +1,20 @@
+package com.education.paymentservice.config;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+
+@Configuration
+public class KafkaTopicConfig {
+
+    @Bean
+    public NewTopic orderReadyForPaymentTopic() {
+        return TopicBuilder.name("order-ready-for-payment").build();
+    }
+
+    @Bean
+    public NewTopic orderStatusUpdatesTopic() {
+        return TopicBuilder.name("order-status-updates").build();
+    }
+}
